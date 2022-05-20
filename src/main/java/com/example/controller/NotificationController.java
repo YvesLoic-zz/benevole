c855a1e9ed77c0192b8507f8aaf88dd236000137
Loic <yvestchoumteu55@gmail.com>
2022-05-20 22:44:15 +0200

    En cours de finitions

---
+++ src/main/java/com/example/controller/NotificationController.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.controller;
+
+import com.example.entities.Notification;
+import com.example.entities.User;
+import com.example.responses.NotificationResponse;
+import com.example.services.NotificationService;
+import com.example.services.UserService;
+import com.example.util.CustomUserDetail;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Yves
+ */
+@Controller
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    private static final String NOTIF_LIST = "notif/list";
+
+    private static final String NOTIFICATIONS = "notifications";
+
+    @Autowired
+    private NotificationService ns;
+
+    @Autowired
+    private UserService us;
+
+    @GetMapping("/index")
+    public String index(@AuthenticationPrincipal CustomUserDetail principal, Model model) {
+        User u = us.one(principal.getUsername()).orElse(null);
+        model.addAttribute(NOTIFICATIONS,
+                ns.all(u.getBenevole()).stream().map(NotificationResponse::new).collect(Collectors.toList()));
+        return NOTIF_LIST;
+    }
+
+    @GetMapping("/{id}/delete")
+    public String destroy(Model model, @PathVariable("id") Long id) {
+        Notification n = ns.one(id).orElse(null);
+        if (n != null) {
+            ns.delete(id);
+            return NOTIF_LIST;
+        }
+        return NOTIF_LIST;
+    }
+}
---
+++ src/main/java/com/example/responses/DistributionResponse.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.responses;
+
+import com.example.entities.Distribution;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Yves
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DistributionResponse implements Serializable {
+
+    private Long id;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String lieux;
+    private String statut;
+    private EquipeResponse equipe;
+    private int effectif;
+    private String rapport;
+
+    public DistributionResponse(Distribution d) {
+        this.id = d.getId();
+        this.startDate = d.getStartDate();
+        this.endDate = d.getEndDate();
+        this.lieux = d.getLocation();
+        this.statut = d.getStatut();
+        this.equipe = new EquipeResponse(d.getEquipe());
+        this.effectif = d.getEquipe().getMembers().size();
+        this.rapport = d.getRapport();
+    }
+}
